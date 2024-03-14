2da28d5e2c81ace6066bd380819f1a7bddfa803b
Gabogogi <bencitymaina@gmail.com>
2024-03-15 01:50:49 +0300

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''Type annotation for a list of floats'''
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''returns sum of list as a float'''
     return sum(input_list)
