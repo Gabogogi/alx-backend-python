3cd9691a67652a9f4cade7edd7561c6d1f9a2167
Gabogogi <bencitymaina@gmail.com>
2024-03-15 00:44:29 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Takes a float and returns the string representation of the float.'''
+
+
+def to_str(n: float) -> str:
+    '''Return the string rep of a float'''
+    return str(n)
