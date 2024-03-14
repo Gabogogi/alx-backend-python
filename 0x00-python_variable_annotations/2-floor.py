c2e0b7cec4113336baa62aee20f9e474e73bb439
Gabogogi <bencitymaina@gmail.com>
2024-03-15 00:44:55 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Takes a float and returns its floor as a float'''
+import math
+
+
+def floor(n: float) -> float:
+    '''Returns te floor of a float'''
+    return math.floor(n)
