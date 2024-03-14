18c43ac829649db285b9b4c8881b47529fca4ac2
Gabogogi <bencitymaina@gmail.com>
2024-03-15 00:43:30 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Type annotation for a list of floats'''
+
+def sum_list(input_list: list[float]) -> float:
+    '''returns sum of list as a float'''
+    return sum(input_list)
