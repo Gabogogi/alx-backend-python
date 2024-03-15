89124051a0a66094c4a8780f2a9e3935e3811618
Gabogogi <bencitymaina@gmail.com>
2024-03-15 23:52:58 +0300

    PEP8 issues

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 '''Annotates a given function'''
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Type annotation for an iterable'''
     return [(i, len(i)) for i in lst]
