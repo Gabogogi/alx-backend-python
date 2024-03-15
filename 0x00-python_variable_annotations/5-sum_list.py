5e35cc05e9d3b546e916d609e3c459a1b0af5413
Gabogogi <bencitymaina@gmail.com>
2024-03-15 23:50:34 +0300

    PEP8 issues

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 '''Type annotation for a list of floats'''
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     '''returns sum of list as a float'''
     return sum(input_list)
