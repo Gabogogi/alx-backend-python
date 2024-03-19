89127816687ca2b6550aee854f24364c6bec2d4a
Gabogogi <bencitymaina@gmail.com>
2024-03-19 12:15:27 +0300

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''wait_n should return the list of all the delays (float values)'''
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''Waits for ran delay until max_delay, returns list of actual delays'''
+    delays: List[float] = []
+    order_list: List[float] = []
+
+    for _ in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    for o in asyncio.as_completed(delays):
+        order_list.append(await o)
+
+    return order_list
