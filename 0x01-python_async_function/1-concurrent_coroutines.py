1a65a0aea6736b5856ff82c6649530de7877d2c0
Gabogogi <bencitymaina@gmail.com>
2024-03-26 19:45:15 +0300

    PEP 8 Issues

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     '''Waits for ran delay until max_delay, returns list of actual delays'''
     spawn_list = []
